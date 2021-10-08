eaa7d9cca76323f7fcc5e9452cc02c647cde47d5
Michael-Kochis <rhishisikk@gmail.com>
2021-10-07 23:18:39 -0500

    Possible fix

--- src/main/java/com/revature/springskeleton/repositories/UserRepository.java
+++ src/main/java/com/revature/springskeleton/repositories/UserRepository.java
@@ -1,8 +1,6 @@
 package com.revature.springskeleton.repositories;
 
 import com.revature.springskeleton.models.SiteUser;
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
