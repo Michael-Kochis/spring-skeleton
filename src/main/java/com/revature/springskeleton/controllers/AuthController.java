112d103309e2fe265a968ca836cdc05ddbc6729a
Michael-Kochis <rhishisikk@gmail.com>
2021-10-07 22:42:14 -0500

    Auth/register test

---
+++ src/main/java/com/revature/springskeleton/controllers/AuthController.java
@@ -0,0 +1,23 @@
+package com.revature.springskeleton.controllers;
+
+import com.revature.springskeleton.models.SiteUser;
+import com.revature.springskeleton.repositories.UserRepository;
+import com.revature.springskeleton.utils.PasswordUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/auth")
+public class AuthController {
+    @Autowired
+    private UserRepository users;
+
+    @PostMapping("/register")
+    public SiteUser makeUser(@RequestBody SiteUser neoUser) {
+        neoUser.setPassword(PasswordUtils.encrypt(neoUser.getPassword()) );
+        return this.users.save(neoUser);
+    }
+
+
+}
---
+++ src/main/java/com/revature/springskeleton/utils/JWTTokenUtils.java
@@ -0,0 +1,4 @@
+package com.revature.springskeleton.utils;
+
+public class JWTTokenUtils {
+}
