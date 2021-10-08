d7de01e8c0761f68b6551f0c27661ddab382e567
Michael-Kochis <rhishisikk@gmail.com>
2021-10-07 21:30:45 -0500

    Testing PUT

--- src/main/java/com/revature/springskeleton/controllers/UserController.java
+++ src/main/java/com/revature/springskeleton/controllers/UserController.java
@@ -1,12 +1,15 @@
 package com.revature.springskeleton.controllers;
 
+import com.revature.springskeleton.exceptions.ResourceNotFoundException;
 import com.revature.springskeleton.models.SiteUser;
 import com.revature.springskeleton.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.ResourceAccessException;
 
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 @CrossOrigin
@@ -22,11 +25,31 @@ public class UserController {
 
     @GetMapping("/{id}")
     public ResponseEntity<SiteUser> getUserByID(@PathVariable(value="id") Long userID)
-            throws Exception {
+            throws ResourceNotFoundException {
         SiteUser user = users.findById(userID)
                 .orElseThrow(
-                        () -> new Exception("Employee not found for ID: " + userID));
+                        () -> new ResourceAccessException("Employee not found for ID: " + userID)
+                );
         return ResponseEntity.ok().body(user);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<SiteUser> updateUser(@PathVariable(value = "id") Long userID,
+        @RequestBody SiteUser user) throws ResourceNotFoundException {
+        SiteUser oldUser = users.findById(userID)
+                .orElseThrow(
+                        () -> new ResourceNotFoundException("Employee not found for ID: " + userID)
+                );
+        if (user.getUsername() != null && !user.getUsername().equals(""))
+            oldUser.setUsername(user.getUsername());
+        if (user.getFirstName() != null && !user.getFirstName().equals(""))
+            oldUser.setFirstName(user.getFirstName());
+        if (user.getLastName() != null && !user.getLastName().equals(""))
+            oldUser.setLastName(user.getLastName());
+        if (user.getEmail() != null && !user.getEmail().equals(""))
+            oldUser.setEmail(user.getEmail());
+
+        return ResponseEntity.ok(this.users.save(oldUser));
     }
 
     @PostMapping("/")
