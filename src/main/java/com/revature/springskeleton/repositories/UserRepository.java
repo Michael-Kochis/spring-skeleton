49843a4ede9f92a35259b66bd3be46fc1d6c19b5
Michael-Kochis <rhishisikk@gmail.com>
2021-10-07 22:57:34 -0500

    WTF Interface default

--- src/main/java/com/revature/springskeleton/repositories/UserRepository.java
+++ src/main/java/com/revature/springskeleton/repositories/UserRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<SiteUser, Long> {
-    public SiteUser findByUsername(String target) {
+    public default SiteUser findByUsername(String target) {
         SiteUser sample = new SiteUser();
         sample.setUsername(target);
 
